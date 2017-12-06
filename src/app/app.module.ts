da7187e6e2c302f77e7a9e2927aaa820c4ec6414
Ruchiran Avishka <ruchiavishka@gmail.com>
2017-12-06 16:42:34 +0530

    made ui for home and sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule
